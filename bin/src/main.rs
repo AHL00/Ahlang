7eafbfacad99e573fc393ea7d194af8f50cc2c9e
Khant Htet <83922982+AHL00@users.noreply.github.com>
2023-07-21 17:03:14 +0000

    REPL History

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -94,8 +94,16 @@ fn last_line() {
 }
 
 
-const continuation_line: &str = ".. ";
-const start_line: &str = "> ";
+const continuation_line: &str = "└─ ";
+const start_line: &str = ">> ";
+
+fn print_line_start() {
+    print!("\x1B[32m{}\x1B[0m", start_line);
+}
+
+fn print_continuation_start() {
+    print!("\x1B[32m{}\x1B[0m", continuation_line);
+}
 
 fn repl() {
     let mut engine = ReplEngine::new();
@@ -104,7 +112,7 @@ fn repl() {
     let mut history_ptr = 0;
 
     println!("\x1B[34mAhlang REPL v{}\x1B[0m", env!("CARGO_PKG_VERSION"));
-    println!("\x1B[32mType 'exit' to exit, 'help' for more information\x1B[0m");
+    println!("\x1B[32mPress Ctrl + Z to exit, Ctrl + H for more information\x1B[0m");
 
     let stdout = io::stdout().into_raw_mode().unwrap();
     let mut stdout = io::BufWriter::new(stdout);
@@ -114,8 +122,8 @@ fn repl() {
     let mut events = stdin.events();
 
     'repl: loop {
-
-        write!(stdout, "\x1B[32m{}\x1B[0m", start_line).unwrap();
+        line_start();
+        print_line_start();
         stdout.flush().unwrap();
 
         let mut input = String::new();
@@ -143,7 +151,7 @@ fn repl() {
                                         // save where in the input this line starts
                                         line_start_locs.push(input.len());
 
-                                        write!(stdout, "{}", continuation_line).unwrap();
+                                        print_continuation_start();
                                         stdout.flush().unwrap();
                                         continue;
                                     }
@@ -153,11 +161,25 @@ fn repl() {
                                 print!("{}", chr);
                                 stdout.flush().unwrap();
                             },
-                            termion::event::Key::Ctrl('c') => {
+                            termion::event::Key::Ctrl('z') => {
                                 next_line_start();
                                 print!("\x1B[33m! Exiting...\x1B[0m");
                                 next_line_start();
                                 break 'repl;
+                            },
+                            termion::event::Key::Ctrl('h') => {
+                                next_line_start();
+                                //print!("\x1B[33m! Help:\n! Ctrl + Z to exit\n! Ctrl + H for help \n To write multiline statements, press enter without ending the line in a semicolon.\x1B[0m");
+                                print!("\x1B[33m");
+                                print!("! Help:");
+                                next_line_start();
+                                print!("- Ctrl + Z to exit");
+                                next_line_start();
+                                print!("- Ctrl + H for help");
+                                next_line_start();
+                                print!("- To write multiline statements, press enter without ending the line in a semicolon.");
+                                next_line_start();
+                                break;
                             },
                             termion::event::Key::Backspace => {
                                 // if we are at the start of the line, don't delete
@@ -176,9 +198,9 @@ fn repl() {
                                         let mut chars_on_line = input.len() - line_start_locs[line_start_locs.len() - 1];
                                         // account for line start
                                         if line_start_locs.len() == 1 {
-                                            chars_on_line += start_line.len();
+                                            chars_on_line += start_line.chars().count();
                                         } else {
-                                            chars_on_line += continuation_line.len();
+                                            chars_on_line += continuation_line.chars().count();
                                         }
 
                                         move_right(chars_on_line as u16);
@@ -193,46 +215,129 @@ fn repl() {
                                 write!(stdout, "\x08 \x08").unwrap();
                                 stdout.flush().unwrap();
                             },
-                            termion::event::Key::Left => {
-                                print!("{}", termion::cursor::Left(1));
-                                stdout.flush().unwrap();
-                            },
-                            termion::event::Key::Right => {
-                                print!("{}", termion::cursor::Right(1));
-                                stdout.flush().unwrap();
-                            },
+                            // termion::event::Key::Left => {
+                            //     print!("{}", termion::cursor::Left(1));
+                            //     stdout.flush().unwrap();
+                            // },
+                            // termion::event::Key::Right => {
+                            //     print!("{}", termion::cursor::Right(1));
+                            //     stdout.flush().unwrap();
+                            // },
                             termion::event::Key::Up => {
+                                // the latest input is at the end of the stack
+                                // if we are at the start of the stack, don't do anything
                                 if history_ptr == 0 {
                                     continue;
                                 }
 
-                                history_ptr -= 1;
-
+                                // clear all multiline input
+                                for _ in 0..line_start_locs.len() - 1 {
+                                    delete_line();
+                                    last_line();
+                                }
                                 delete_line();
+
+                                // move to start of line
                                 line_start();
 
-                                input = history_stack[history_ptr].clone();
-                                print!("{}", input);
+                                // seperate input by \n
+                                let line: Vec<&str> = history_stack[history_ptr - 1].split('\n').collect();
+                                
+                                // fill line_start_locs with the start of each line
+                                line_start_locs.clear();
+                                line_start_locs.push(0);
+
+                                for (i, c) in history_stack[history_ptr - 1].char_indices() {
+                                    if c == '\n' {
+                                        line_start_locs.push(i + 1);
+                                    }
+                                }
+                                
+                                // first line
+                                print_line_start();
+                                write!(stdout, "{}", line[0]).unwrap();
                                 stdout.flush().unwrap();
+                                
+                                // other lines
+                                for i in 1..line.len() {
+                                    next_line_start();
+                                    print_continuation_start();
+                                    write!(stdout, "{}", line[i]).unwrap();
+                                    stdout.flush().unwrap();
+                                }
+
+
+                                // set input to history stack string
+                                input = history_stack[history_ptr - 1].clone();
+
+                                // move the stack pointer up
+                                if history_ptr > 0 {
+                                    history_ptr -= 1;
+                                }
                             },
                             termion::event::Key::Down => {
+                                // if we are at the end of the stack, don't do anything
                                 if history_ptr == history_stack.len() {
                                     continue;
                                 }
 
-                                history_ptr += 1;
+                                if history_ptr == history_stack.len() - 1 {
+                                    // clear all multiline input
+                                    for _ in 0..line_start_locs.len() - 1 {
+                                        delete_line();
+                                        last_line();
+                                    }
+                                    delete_line();
 
-                                delete_line();
-                                line_start();
+                                    // clear input
+                                    input.clear();
 
-                                if history_ptr == history_stack.len() {
-                                    input = String::new();
-                                } else {
-                                    input = history_stack[history_ptr].clone();
+                                    break;
                                 }
 
-                                print!("{}", input);
+                                // clear all multiline input
+                                for _ in 0..line_start_locs.len() - 1 {
+                                    delete_line();
+                                    last_line();
+                                }
+                                delete_line();
+
+                                // move to start of line
+                                line_start();
+
+                                // seperate input by \n
+                                let line: Vec<&str> = history_stack[history_ptr + 1].split('\n').collect(); 
+
+                                // fill line_start_locs with the start of each line
+                                line_start_locs.clear();
+                                line_start_locs.push(0);
+
+                                for (i, c) in history_stack[history_ptr + 1].char_indices() {
+                                    if c == '\n' {
+                                        line_start_locs.push(i + 1);
+                                    }
+                                }
+
+                                // first line
+                                print_line_start();
+                                write!(stdout, "{}", line[0]).unwrap();
                                 stdout.flush().unwrap();
+
+                                // other lines
+                                for i in 1..line.len() {
+                                    next_line_start();
+                                    print_continuation_start();
+                                    write!(stdout, "{}", line[i]).unwrap();
+                                    stdout.flush().unwrap();
+                                }
+
+                                // set input to history stack string
+                                input = history_stack[history_ptr + 1].clone();
+
+                                // move the stack pointer down
+                                if history_ptr < history_stack.len() {
+                                    history_ptr += 1;
+                                }
                             },
                             _ => {}
                         }
@@ -244,24 +349,14 @@ fn repl() {
 
         input = input.trim().to_owned();
 
+        if input == "" {
+            continue;
+        }
+        
+        history_stack.push(input.clone());
+    
         if input.starts_with("//") {
             continue;
-        }
-
-        if input == "exit" {
-            write!(stdout, "\x1B[31m! Exiting\x1B[0m").unwrap();
-            break;
-        }
-
-        if input == "help" {
-            println!("Type 'exit' to exit, 'help' for more information");
-            println!("If a line ends with a semicolon, it will be evaluated immediately.");
-            println!("Otherwise, the statement will be continued on the next line with '.. '");
-            continue;
-        }
-
-        if !input.is_empty() {
-            history_stack.push(input.clone());
         }
 
         let res = engine.eval(input.as_str());

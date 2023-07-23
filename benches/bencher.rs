a5c1b51eab8db081a0f4ff97682873ebc6f58c80
Ahloo <khanthtetaung.khanthtet@gmail.com>
2023-07-24 01:57:03 +0630

    Update bencher to validate results

--- benches/bencher.rs
+++ benches/bencher.rs
@@ -1,7 +1,7 @@
 // Benchmark macro, which runs a script and reports the time taken in each stage
-
-macro_rules! define_benchmark {
-    ($name:ident, $code:expr) => {
+#[allow(unused_macros)]
+macro_rules! benchmark {
+    ($name:ident, $code:expr, [$(($var_name:expr, $expected:expr)),*]) => {
         use criterion::{criterion_group, criterion_main, Criterion};
         use ahlang::{*, lexer::Tokens, parser::Ast};
         fn lexer_bench(script_: &str) {
@@ -22,17 +22,49 @@ macro_rules! define_benchmark {
         }
 
         fn $name(c: &mut Criterion) {
-            c.bench_function("lexer", |b| b.iter(|| lexer_bench($code)));
-
+            
             let mut lexer = Lexer::new();
             lexer.set_input($code);
-            let tokens = lexer.tokenize().unwrap();
-
-            c.bench_function("parser", |b| b.iter(|| parser_bench(&tokens)));
-
+            let tokens = lexer.tokenize().expect("Lexer failed");
+            
             let mut parser = Parser::new();
             parser.set_tokens(&tokens);
-            let ast = parser.parse().unwrap();
+            let ast = parser.parse().expect("Parser failed");
+            
+            let mut interpreter = Interpreter::new();
+            interpreter.run(&ast).unwrap();
+            
+            let vars = interpreter.dbg_get_vars();
+
+            // validate results
+            $(
+                let actual_value = &vars.iter().find(|(k, _)| k == $var_name).unwrap().1;
+                match ($expected, actual_value.clone()) {
+                    (ahlang::Data::Int32 {val: expected}, ahlang::Data::Int32 {val: actual}) => {
+                        assert_eq!(expected, actual, "Variable {} does not match expected value", $var_name);
+                    },
+                    (ahlang::Data::Float64 {val: expected}, ahlang::Data::Float64 {val: actual}) => {
+                        assert_eq!(expected, actual, "Variable {} does not match expected value", $var_name);
+                    },
+                    (ahlang::Data::Bool {val: expected}, ahlang::Data::Bool {val: actual}) => {
+                        assert_eq!(expected, actual, "Variable {} does not match expected value", $var_name);
+                    },
+                    (ahlang::Data::Char {val: expected}, ahlang::Data::Char {val: actual}) => {
+                        assert_eq!(expected, actual, "Variable {} does not match expected value", $var_name);
+                    },
+                    (ahlang::Data::Str {val: expected}, ahlang::Data::Str {val: actual}) => {
+                        assert_eq!(expected, actual, "Variable {} does not match expected value", $var_name);
+                    },
+                    _ => {
+                        panic!("Variable {} has unexpected type", $var_name);
+                    },
+                };
+            )*
+
+
+            c.bench_function("lexer", |b| b.iter(|| lexer_bench($code)));
+
+            c.bench_function("parser", |b| b.iter(|| parser_bench(&tokens)));
 
             c.bench_function("eval", |b| b.iter(|| eval_bench(&ast)));
         }
--- benches/while_iter.rs
+++ benches/while_iter.rs
@@ -1,11 +1,21 @@
 #[macro_use]
 mod bencher;
 
+#[macro_use]
+mod tester;
+
 const SCRIPT: &str = r#"
-let a: i32 = 0;
-while a < 1000000 {
-    a = a + 1;
+let x: i32 = 0;
+let y: bool = false;
+let iters: i32 = 0;
+
+while x < 100 {
+    iters = iters + 1;
+    if y {
+        x = x + 1;
+    }
+    y = !y;
 }
 "#;
 
-define_benchmark!(while_iter, SCRIPT);
+benchmark!(while_iter, SCRIPT, [("x", Data::Int32 { val: 100 }), ("y", Data::Bool { val: false }), ("iters", Data::Int32 { val: 200 })]);
